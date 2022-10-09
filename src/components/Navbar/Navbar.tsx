51f8956dd7ce612d3abc24bc49d18b0c94fc2680
jocanm <amjose09@gmail.com>
2022-10-09 17:37:09 -0500

    navbar image url changed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,8 +4,7 @@ import './Navbar.scss'
 export const Navbar = () => {
     return (
         <div className='navbar__wrapper'>
-            <img src="/assets/pufi.png" alt="pufi" />
+            <img src="/assets/Pufi.png" alt="pufi" />
         </div>
     )
 }
-
